c1251a4f7173ff08f2fe1f0024b3b55c0f6c70cc
WhyMChibisova <chibisova3586@gmail.com>
2026-03-24 21:16:21 +0300

    fix: split main class into separate classes

--- src/main/java/org/example/module2/Main.java
+++ src/main/java/org/example/module2/Main.java
@@ -1,46 +1,17 @@
 package org.example.module2;
 
-import org.example.module2.model.Book;
 import org.example.module2.model.Student;
+import org.example.module2.util.StudentReader;
+import org.example.module2.util.StudentStream;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     private static final String FILE_NAME = "src/main/resources/students.json";
 
     public static void main(String[] args) {
-        Path fileStudents = Paths.get(FILE_NAME);
-        List<Student> students = null;
+        List<Student> students = StudentReader.readStudentsFromJsonFile(FILE_NAME);
 
-        if (Files.exists(fileStudents) && Files.isRegularFile(fileStudents)) {
-            students = StudentReader.readStudentsFromFile(fileStudents);
-        } else {
-            System.out.println("File not found: " + fileStudents.toAbsolutePath());
-            return;
-        }
-
-        if (students == null || students.isEmpty()) {
-            System.out.println("File is empty");
-        } else {
-            students.stream()
-                    .peek(System.out::println)
-                    .map(Student::getBooks)
-                    .flatMap(Collection::stream)
-                    .sorted(Comparator.comparingInt(Book::getNumberOfPages))
-                    .distinct()
-                    .filter(book -> book.getYearOfRelease() < 2000)
-                    .limit(3)
-                    .map(Book::getYearOfRelease)
-                    .findFirst()
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Book not found")
-                    );
-        }
+        StudentStream.processStudentData(students);
     }
 }
---
+++ src/main/java/org/example/module2/util/StudentReader.java
@@ -0,0 +1,33 @@
+package org.example.module2.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.module2.model.Student;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class StudentReader {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static List<Student> readStudentsFromJsonFile(String filename) {
+        Path fileStudents = Paths.get(filename);
+        List<Student> students = null;
+
+        if (Files.exists(fileStudents) && Files.isRegularFile(fileStudents)) {
+            try (BufferedReader reader = Files.newBufferedReader(fileStudents, StandardCharsets.UTF_8)) {
+                students = objectMapper.readValue(reader, new TypeReference<List<Student>>() {});
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        } else {
+            System.out.println("File not found: " + fileStudents.toAbsolutePath());
+        }
+        return students;
+    }
+}
